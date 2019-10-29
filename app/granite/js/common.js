0b77b7d4f11209e227ab3bef4ba46d75cd967738
meln1337 <53012422+meln1337@users.noreply.github.com>
2019-10-29 13:46:39 +0200

    Fix nav granite

--- app/granite/js/common.js
+++ app/granite/js/common.js
@@ -6,8 +6,12 @@ $('.mobile-menu').on('click', function (e) {
     e.preventDefault();
     $('.menu-btn').toggleClass('menu-active');
     $('.nav').css('opacity', 1)
+    $('body').css('height', '100vh')
+    $('body').css('position', 'fixed')
   } else {
     $('.nav').css('opacity', 0);
+    $('body').css('height', 'auto')
+    $('body').css('position', 'block')
     a = 0;
     $('.menu-btn').toggleClass('menu-active');
   }
