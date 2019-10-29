4775aa88641bb346a95b7fad92b528aa2685a74e
meln1337 <53012422+meln1337@users.noreply.github.com>
2019-10-29 16:03:02 +0200

    Update common.js

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,18 @@
-$(function() {
+var a = 0;
 
-	// Custom JS
-
-});
+$('.mobile-menu').on('click', function (e) {
+  if (a == 0) {
+    a = 1;
+    e.preventDefault();
+    $('.menu-btn').toggleClass('menu-active');
+    $('.nav').css('opacity', 1);
+    $('body').css('height', '100vh');
+    $('body').css('position', 'fixed');
+  } else {
+    $('.nav').css('opacity', 0);
+    $('body').css('height', 'auto');
+    $('body').css('position', 'block');
+    a = 0;
+    $('.menu-btn').toggleClass('menu-active');
+  }
+})
